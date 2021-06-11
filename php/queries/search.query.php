8352986409f8d66b09e3966fb7ca8d32368ea7a5
Erik Morbach <morbacherik@gmail.com>
2021-06-10 22:59:27 -0300

    movendo querys e reformulando com funções

--- php/queries/search.query.php
+++ php/queries/search.query.php
@@ -1,6 +1,8 @@
 <?php
-    $response = NULL;
-    if(isset($_POST["description"])){
+    function search($description){
+        if(session_status()!=PHP_SESSION_ACTIVE) return false;
+        if(!isset($description)) return false;
+
         require("../connection.php");
         
         $inputProductDescription = $_POST["description"];
@@ -8,7 +10,10 @@
         $query = "select * from product p where p.description like lower(\"%$inputProductDescription%\")";
         if($result = $connection->query($query)){
             $response = $result->fetch_all(MYSQLI_ASSOC);
+            $_SESSION["resultProductList"] = $response; 
+            return true;
         }
         $connection->close();
+        return false;
     }
 ?>
